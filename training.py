4e57100e34f07182988adede77f9265b3ec1ef7c
Richard Anton <ranton@ranton.org>
2021-05-27 19:26:29 -0700

    Add dataloaders parameter to train_model.

--- training.py
+++ training.py
@@ -11,8 +11,8 @@ import torch
 # TODO: move find learning rate stuff here.
 
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    """Train a PyTorch model given criterion(loss), optimizer, learning rate scheduler."""
+def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=25):
+    """Train a PyTorch model given model, dataloaders, criterion(loss), optimizer, learning rate scheduler."""
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
